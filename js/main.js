a66630a562f2d29b1ebb80d2ce203df18a4581cd
Rebecca Elizabeth Tay <rebecca@Rebeccas-MacBook-Pro.local>
2017-04-15 10:29:50 +0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+cardOne = "queen";
+cardTwo = "queen";
+cardThree = "king";
+cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
