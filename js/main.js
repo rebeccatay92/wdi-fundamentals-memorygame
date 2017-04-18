a34c5e854c87d69ebfb710a51bb4ec06fe6fbb73
Rebecca Elizabeth Tay <rebecca@Rebeccas-MacBook-Pro.local>
2017-04-19 00:45:44 +0800

    flipCard and createBoard not working. board disappeared when moved in js.

--- js/main.js
+++ js/main.js
@@ -26,6 +26,7 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function () {
+	document.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You've found a match!")
@@ -33,7 +34,9 @@ var checkForMatch = function () {
 	}	
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	/*error: this.getAttribute is not a function?*/
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
@@ -41,7 +44,16 @@ var flipCard = function (cardId) {
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (i = 0; i < cards.length; i ++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener("click", flipCard());
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
+
 
 
